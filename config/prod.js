ed2487b92c94d48edc6a57cffa87fd4e2971a0e7
alonWohl <alonw1010@gmail.com>
2024-12-24 13:23:48 +0200

    Connect to atlas

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-	dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+	dbURL: 'mongodb+srv://alonw1010:12345@cluster0.nqv7u.mongodb.net/',
 	dbName: 'toy_db'
 }
